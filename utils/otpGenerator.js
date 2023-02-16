f02783ceccc41a372208789690a74194809d773d
Srivastava-Tanya <tanyadi2002@gmail.com>
2023-02-16 19:34:00 +0530

    add logic to generate otp

---
+++ utils/otpGenerator.js
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import otpGenerator from "otp-generator";
+
+const generateOTP = () => {
+	const otp = otpGenerator.generate(8, {
+		alphabets: false,
+		upperCase: false,
+		specialChars: false,
+	});
+
+	const now = new Date();
+	const otpexpiration = now.getUTCSeconds() + 600;
+	return [otp, otpexpiration];
+};
+
+export default generateOTP;
